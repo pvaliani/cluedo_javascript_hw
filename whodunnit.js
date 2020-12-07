508e77f332cab45d22d0b28f5430c8d9190f1315
Pedram Valiani <p.valiani@gmail.com>
2020-12-07 18:33:14 +0000

    first commit

---
+++ whodunnit.js
@@ -0,0 +1,73 @@
+// EPISODE 1
+
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Library',
+    weapon: 'Rope'
+  };
+  
+  const declareMurderer = function() {
+    return `The murderer is ${scenario.murderer}.`;
+  }
+  
+  const verdict = declareMurderer();
+  console.log(verdict);
+
+
+//  EPISODE 1 ------------------------------------
+
+
+
+// EPISODE 2
+
+
+
+//  EPISODE 2 ------------------------------------
+
+
+// EPISODE 3
+
+
+
+//  EPISODE 3 ------------------------------------
+
+
+// EPISODE 4
+
+
+
+//  EPISODE 4 ------------------------------------
+
+
+// EPISODE 5
+
+
+
+//  EPISODE 5 ------------------------------------
+
+
+
+// EPISODE 6
+
+
+
+//  EPISODE 6 ------------------------------------
+
+
+// EPISODE 7
+
+
+
+//  EPISODE  7 ------------------------------------
+
+
+// EPISODE 8
+
+
+
+//  EPISODE 8 ------------------------------------
+
+
+
+
+
