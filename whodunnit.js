02da1dada66e231c722214cccc1b3ac48bb1c8ff
Pedram Valiani <p.valiani@gmail.com>
2020-12-07 19:21:08 +0000

    finished MVP

--- whodunnit.js
+++ whodunnit.js
@@ -1,17 +1,17 @@
 // EPISODE 1
 
-// const scenario = {
-//     murderer: 'Miss Scarlet',
-//     room: 'Library',
-//     weapon: 'Rope'
-//   };
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Library',
+    weapon: 'Rope'
+  };
   
-//   const declareMurderer = function() {
-//     return `The murderer is ${scenario.murderer}.`;
-//   }
+  const declareMurderer = function() {
+    return `The murderer is ${scenario.murderer}.`;
+  }
   
-//   const verdict = declareMurderer();
-//   console.log(verdict);
+  const verdict = declareMurderer();
+  console.log(verdict);
 
 // MY PREDICTION: The verdict will be Miss Scarlet as murderer. The const declareMurderer is passed to verdict once it has been executed.
 
@@ -22,19 +22,19 @@
 
 // EPISODE 2
 
-// const murderer = 'Professor Plum';
+const murderer = 'Professor Plum';
 
-// const changeMurderer = function() {
-//   murderer = 'Mrs. Peacock';
-// }
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
 
-// const declareMurderer = function() {
-//   return `The murderer is ${murderer}.`;
-// }
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
 
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
 
 // MY PREDICTION: Error due to reassignment of constant variable 'murderer'
 
@@ -44,18 +44,18 @@
 
 // EPISODE 3
 
-// let murderer = 'Professor Plum';
+let murderer = 'Professor Plum';
 
-// const declareMurderer = function() {
-//   let murderer = 'Mrs. Peacock';
-//   return `The murderer is ${murderer}.`;
-// }
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
 
-// const firstVerdict = declareMurderer();
-// console.log('First Verdict: ', firstVerdict);
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
 
-// const secondVerdict = `The murderer is ${murderer}.`;
-// console.log('Second Verdict: ', secondVerdict);
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
 
 // MY PREDICTION: first verdict will be Mrs Peacock and second will be Professor plum as Mrs Peacock is "temporarily"/locally stored within the declareMurderer method and Professor plum is available globally outside of a function 
 
@@ -65,18 +65,18 @@
 
 // EPISODE 4
 
-// let suspectOne = 'Miss Scarlet';
-// let suspectTwo = 'Professor Plum';
-// let suspectThree = 'Mrs. Peacock';
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
 
-// const declareAllSuspects = function() {
-//   let suspectThree = 'Colonel Mustard';
-//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
-// }
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
 
-// const suspects = declareAllSuspects();
-// console.log(suspects);
-// console.log(`Suspect three is ${suspectThree}.`);
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
 
 // MY PREDICTION: All suspects will be passed to declareAllSuspects. SuspectThree will be printed out in the console log as its value defined in the 3rd line of code
 
@@ -87,25 +87,25 @@
 
 // EPISODE 5
 
-// const scenario = {
-//     murderer: 'Miss Scarlet',
-//     room: 'Kitchen',
-//     weapon: 'Candle Stick'
-//   };
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Kitchen',
+    weapon: 'Candle Stick'
+  };
 
 
   
-//   const changeWeapon = function(newWeapon) {
-//     scenario.weapon = newWeapon;
-//   }
+  const changeWeapon = function(newWeapon) {
+    scenario.weapon = newWeapon;
+  }
   
-//   const declareWeapon = function() {
-//     return `The weapon is the ${scenario.weapon}.`;
-//   }
+  const declareWeapon = function() {
+    return `The weapon is the ${scenario.weapon}.`;
+  }
   
-//   changeWeapon('Revolver');
-//   const verdict = declareWeapon();
-//   console.log(verdict);
+  changeWeapon('Revolver');
+  const verdict = declareWeapon();
+  console.log(verdict);
 
 //   MY PREDICTION: scenario.weapon i.e the object attribute will be reassigned the newWeapon in the function definition. The result will be Revolver
 
@@ -116,25 +116,25 @@
 
 // EPISODE 6
 
-// let murderer = 'Colonel Mustard';
+let murderer = 'Colonel Mustard';
 
-// const changeMurderer = function() {
-//   murderer = 'Mr. Green';
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
 
-//   const plotTwist = function() {
-//     murderer = 'Mrs. White';
-//   }
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
 
-//   plotTwist();
-// }
+  plotTwist();
+}
 
-// const declareMurderer = function () {
-//   return `The murderer is ${murderer}.`;
-// }
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
 
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
 
 // MY PREDICTION: Mrs White - plot twist method call means that within the change murderer function there is a change of murderer and the declareMurderer method will be updated.
 
@@ -179,11 +179,65 @@ console.log(verdict);
 
 // EPISODE 8
 
+const scenario = {
+    murderer: 'Mrs. Peacock',
+    room: 'Conservatory',
+    weapon: 'Lead Pipe'
+  };
+  
+  const changeScenario = function() {
+    scenario.murderer = 'Mrs. Peacock';
+    scenario.room = 'Dining Room';
+  
+    const plotTwist = function(room) {
+      if (scenario.room === room) {
+        scenario.murderer = 'Colonel Mustard';
+      }
+  
+      const unexpectedOutcome = function(murderer) {
+        if (scenario.murderer === murderer) {
+          scenario.weapon = 'Candle Stick';
+        }
+      }
+  
+      unexpectedOutcome('Colonel Mustard');
+    }
+  
+    plotTwist('Dining Room');
+  }
+  
+  const declareWeapon = function() {
+    return `The weapon is ${scenario.weapon}.`
+  }
+  
+  changeScenario();
+  const verdict = declareWeapon();
+  console.log(verdict);
 
+//   MY PREDICTION: Candle Stick
 
 //  EPISODE 8 ------------------------------------
 
 
 
+// EPISODE 9
 
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// MY PREDICTION: Professor Plum as let murderer is only making Mrs Peacock the murderer within the function
+
+
+
+// EPISODE 9----------------------------
 
